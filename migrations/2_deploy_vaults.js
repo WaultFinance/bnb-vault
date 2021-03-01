408033661f45e210c8dc958d29e73684a9a40b48
Warlord-dev <lingdar0719@gmail.com>
2021-03-01 13:15:10 +0300

    deploy

--- migrations/2_deploy_vaults.js
+++ migrations/2_deploy_vaults.js
@@ -3,7 +3,6 @@ const WaultVaultBNB = artifacts.require("WaultVaultBNB");
 
 module.exports = function (deployer) {
 
-  deployer.deploy(RewardsStrategy, "0xFA3CcB086Bf371A2Ff33Db8521Be47c5B4b9d10E", "moo Venus BNB", "mooVenusBNB", 172800);
-  // deployer.deploy(WaultVaultBNB, "0xFA3CcB086Bf371A2Ff33Db8521Be47c5B4b9d10E", "moo Venus BNB", "mooVenusBNB", 172800);
+  deployer.deploy(RewardsStrategy, "0xFA3CcB086Bf371A2Ff33Db8521Be47c5B4b9d10E", "waultReawardBNB", "maultReaward BNB", 172800);
 
 };
