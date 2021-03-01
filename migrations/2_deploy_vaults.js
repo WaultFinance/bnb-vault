cee0b39576b7f7968009126155d498a6197ded99
Warlord-dev <lingdar0719@gmail.com>
2021-03-01 13:22:27 +0300

    deploy

--- migrations/2_deploy_vaults.js
+++ migrations/2_deploy_vaults.js
@@ -1,8 +1,8 @@
-const RewardsStrategy = artifacts.require("RewardsStrategy");
 const WaultBnbStrategy = artifacts.require("WaultBnbStrategy");
+const WaultVaultBNB = artifacts.require("WaultVaultBNB");
 
 module.exports = function (deployer) {
 
-  deployer.deploy(RewardsStrategy, "0xFA3CcB086Bf371A2Ff33Db8521Be47c5B4b9d10E", "waultReawardBNB", "maultReaward BNB", 172800);
+  deployer.deploy(WaultVaultBNB, "0xFA3CcB086Bf371A2Ff33Db8521Be47c5B4b9d10E", "Wault Vault BNB", "WaultVaultBNB", 172800);
 
 };
