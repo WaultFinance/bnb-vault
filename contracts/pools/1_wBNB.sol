d908eb2b5f2ddc564d71fe05c04fc2dd80c0fe1b
Warlord-dev <lingdar0719@gmail.com>
2021-03-02 21:27:46 +0300

    updates

---
+++ contracts/pools/1_wBNB.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.5.16;
+
+import "@openzeppelin/contracts/GSN/Context.sol";
+import "../Wault/IRewardDistributionRecipient.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "../Wault/LPTokenWrapper.sol";
+import "../Wault/RewardPool.sol";
--- migrations/2_deploy_vaults.js
+++ migrations/2_deploy_vaults.js
@@ -3,6 +3,7 @@ const WaultVaultBNB = artifacts.require("WaultVaultBNB");
 
 module.exports = function (deployer) {
 
-  deployer.deploy(WaultVaultBNB, "0xFA3CcB086Bf371A2Ff33Db8521Be47c5B4b9d10E", "Wault Vault BNB", "WaultVaultBNB", 172800);
+  deployer.deploy(WaultBnbStrategy);
+  // deployer.deploy(WaultVaultBNB, "0xFA3CcB086Bf371A2Ff33Db8521Be47c5B4b9d10E", "Wault Vault BNB", "WaultVaultBNB", 172800);
 
 };
